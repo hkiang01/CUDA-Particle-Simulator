319629cf8d485f8ffa730e1c177e6781e0a51d93
hkiang01 <hkiang01@yahoo.com>
2015-11-25 19:32:10 -0600

    pSimLEQ2 compiles

---
+++ pSimLEQ2/pSimLEQ2/particlesystemcuda.cu
@@ -0,0 +1,13 @@
+#include "vectors.h"
+#include "particlesystemcuda.h"
+
+
+#define cudaCheck(stmt) do {													\
+	cudaError_t err = stmt;														\
+	if (err != cudaSuccess) {													\
+		fprintf(stderr, "Failed to run stmt ", #stmt);							\
+		fprintf(stderr, "Got CUDA error ... %s\n", cudaGetErrorString(err));	\
+			}																			\
+} while (0);
+
+
